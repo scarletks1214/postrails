c93cb162674899a7344df242e311a6542644b721
scarletks1214 <scarlet.ks1214@gmail.com>
2019-02-14 07:23:31 +0300

    use the resource generator for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_022253) do
+ActiveRecord::Schema.define(version: 2019_02_14_040017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2019_02_14_022253) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.date "date"
+    t.text "rationale"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
