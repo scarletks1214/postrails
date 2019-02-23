9973d16ae34d56df242b2c0a668225f82cc16d47
scarlet.ks1214 <scarlet.ks1214@gmail.com>
2019-02-23 18:08:53 +0300

    implemented initial view code for audit logs

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  resources :audit_logs
+  resources :audit_logs, except: [:new, :edit, :destory]
+  
   namespace :admin do
-      resources :posts
-      resources :users
-      resources :blogs
-      resources :admin_users
+    resources :users
+		resources :posts
+		resources :admin_users
 
-      root to: "posts#index"
-    end
+    root to: "users#index"
+  end
+
   resources :posts
-  devise_for :users
-  root to: 'static#home'
-  resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, skip: [:registrations]
+  root to: 'static#homepage'
 end
--- spec/features/admin_dashboard_spec.rb
+++ spec/features/admin_dashboard_spec.rb
@@ -1,13 +1,6 @@
-# spec/features/admin_dashboard_spec.rb
-
 require 'rails_helper'
 
 describe 'admin dashboard' do
-  # it 'can be reached successfully' do
-  #   visit admin_root_path
-  #   expect(page.status_code).to eq(200)
-  # end
-
   it 'does not allow users to access without being signed in' do
     visit admin_root_path
     expect(current_path).to eq(new_user_session_path)
@@ -30,5 +23,4 @@ describe 'admin dashboard' do
 
     expect(current_path).to eq(admin_root_path)
   end
-
 end
