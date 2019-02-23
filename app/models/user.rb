a8aadb43349dd2dbfced1ea0ce985a2d50b73253
scarlet.ks1214 <scarlet.ks1214@gmail.com>
2019-02-23 16:40:35 +0300

    created resource for Audit log with model relationshop connections

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :audit_logs
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -12,7 +13,7 @@ class User < ApplicationRecord
   validates_format_of :phone, with: PHONE_REGEX
 
   validates :phone, length: { is: 10 }
-  
+
   def full_name
   	last_name.upcase + ", " + first_name.upcase
   end
---
+++ spec/factories/audit_logs.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :audit_log do
+    user nil
+    status 1
+    start_date "MyString"
+    end_date "2019-02-23"
+  end
+end
