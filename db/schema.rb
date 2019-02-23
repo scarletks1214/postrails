1a467cd2a582c50ea8d3392e23a5c125738d3a0a
scarlet.ks1214 <scarlet.ks1214@gmail.com>
2019-02-23 17:35:04 +0300

    integrated validations and defaults for audit log attributes

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_02_23_133412) do
   create_table "audit_logs", force: :cascade do |t|
     t.bigint "user_id"
     t.integer "status", default: 0
-    t.string "start_date"
+    t.date "start_date"
     t.date "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
